fe0e06de8efe3f0031789925b11ee9fa0f1f9e4d
Pratik <patilpratikp035@gmail.com>
2024-06-12 19:08:45 +0530

    enhancing operaion option

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@ using namespace std;
 int main(){
 
     // decalring variable for numbers and operation 
-    string operation;
+    int operation;
     double num1, num2;
 
     // taking input from user 
     cout<<"Enter First Number: ";
     cin>>num1;
-    cout<<"Enter Type Of Operation (+, -, *, /): ";
+    cout<<"Choose the operation from the following and enter relative number: "<<endl;
+    cout<<"1. Addition"<<endl;
+    cout<<"2. Substraction"<<endl;
+    cout<<"3. Multiplication"<<endl;
+    cout<<"4. Division"<<endl;
     cin>>operation;
-    if(operation=="/"){
+    if(operation==4){
         cout<<"Enter Second Number except 0: ";
         cin>>num2;
         if(num2==0){
@@ -28,16 +32,16 @@ int main(){
     
 
     // maths for operation 
-    if(operation =="+"){
-        cout<<"Answer Of Problem ("<<num1<<" + "<<num2<<") is = "<<num1+num2<<"."<<endl;
+    if(operation ==1){
+        cout<<"Answer Of the Problem ("<<num1<<" + "<<num2<<") is = "<<num1+num2<<"."<<endl;
     }
-    else if(operation =="-"){
+    else if(operation ==2){
         cout<<"Answer Of Problem ("<<num1<<" - "<<num2<<") is = "<<num1-num2<<"."<<endl;
     }
-    else if(operation =="*"){
+    else if(operation ==3){
         cout<<"Answer Of Problem ("<<num1<<" * "<<num2<<") is = "<<num1*num2<<"."<<endl;
     }
-    else if(operation =="/"){
+    else if(operation ==4){
         cout<<"Answer Of Problem ("<<num1<<" / "<<num2<<") is = "<<num1/num2<<"."<<endl;
     }
     else cout<<"You have entered wrong operation."<<endl;
